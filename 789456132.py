cedd846da775b835e4b787ca8516c57fc897823d
五档起步杰 <879303561@qq.com>
2018-07-28 21:10:16 +0800

    login

---
+++ 789456132.py
@@ -0,0 +1,31 @@
+url = "https://jianli.58.com/resumedetail/singles/3_nerXTGys_EmpnedNTvtalEDkTenX_EdNTvUknpsfnenY_EHY_eHYnetvlEOsnErsTisfTErf_6**?psid=175831697200904694340757030&entinfo=3_nerXTGys_EmpnedNTvtalEDkTenX_EdNTvUknpsfnenY_EHY_eHYnetvlEOsnErsTisfTErf_6**_p&sourcepath=pc-jllistb-zhineng&f=pc_list_detai&dpid=a97c9698d877491d9548bcd94982a6dc&followparam=%7B%22searchID%22%3A%22d44165bb5e41405c8a49b875450c86d3%22%2C%22searchVersion%22%3A10000%2C%22searchAreaID%22%3A4%2C%22searchFirstAreaID%22%3A4%2C%22searchPositionID%22%3A2080%2C%22searchSecondPositionID%22%3A2080%2C%22page%22%3A1%2C%22location%22%3A6%2C%22resumeType%22%3A1%2C%22platform%22%3A%22pc%22%2C%22sourcePage%22%3A%22pc-jllistb-zhineng%22%2C%22operatePage%22%3A%22list%22%7D&PGTID=0d302409-0000-45b0-f68b-534624d67d1f&ClickID=3"
+login = "https://passport.58.com/login/pc/dologin"
+headers = {
+# 'cookie': '58home=sz; id58=c5/njVtbE9gULTqkA6GPAg==; city=sz; 58tj_uuid=5c86ce3b-fc9b-4af0-b32d-d5effffd0462; als=0; show_zcm_banner=true; wmda_uuid=984a87ea74f2b228e5c203f73c0478e0; wmda_new_uuid=1; wmda_visited_projects=%3B1731916484865; param8616=1; param8716kop=1; xxzl_deviceid=WCziqwHk3F1vSMqKSdxj2UU7dw0%2B0acz3NsN4oOSZDa5GnNO7cw4NuepKreYWAl3; showPTTip=1; ljrzfc=1; xxzl_smartid=c791fd3aee1e8c89f5114405d25c729; new_uv=2; utm_source=; spm=; init_refer=https%253A%252F%252Fwww.baidu.com%252Flink%253Furl%253DgLKK9TgNqSzdoGvdocBihr7gZKXMt-9Zl9panxmL2SC%2526wd%253D%2526eqid%253Db29e6bf40003e2fd000000065b5c6242; new_session=0; wmda_session_id_1731916484865=1532781167663-62a86c36-6f93-387a; sessionid=536d1c3b-adb8-432d-ae6c-c7b0a977066d; ppStore_fingerprint=33062264A11B40B7017592C42E30536DFE0CCEF6EB36E382%EF%BC%BF1532781184384; PPU="UID=57150970846999&UN=wudangqibujie&TT=226f0b6c8bb6f2ed1c60884f90d04563&PBODY=ZatJaEDaU4B7tKlzXKfFFPZpvtKcy5jAqtdsNf7vs6v8s5wmLjXcmLfi1BgL7TuNgwizZCVuvx1Ckd3JespE0ZraS9DfV79E6HvjuctzgJMdU-5qJQaQtBYsY-NaPchfGtWf0jswZSjIBrFTyO3S8QpEq_1YLiW6vrL0a2VVyUU&VER=1"; 58cooper="userid=57150970846999&username=wudangqibujie&cooperkey=b1dff14e554e73aa5c50441bab756ec5"; www58com="AutoLogin=false&UserID=57150970846999&UserName=wudangqibujie&CityID=0&Email=&AllMsgTotal=0&CommentReadTotal=0&CommentUnReadTotal=0&MsgReadTotal=0&MsgUnReadTotal=0&RequireFriendReadTotal=0&RequireFriendUnReadTotal=0&SystemReadTotal=0&SystemUnReadTotal=0&UserCredit=0&UserScore=0&PurviewID=&IsAgency=false&Agencys=null&SiteKey=887EB60F8846773DB3E02F3D612981162BA6074C29DCA62CA&Phone=&WltUrl=&UserLoginVer=3FFB2E44A074375AA8C94DBBB4CFE61FE&LT=1532781185190"',
+'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.75 Safari/537.36',
+}
+import requests
+data = {
+'password':'296b5eebf8715f710adce67195d413c0d97bcbe717fac6c88e8f2794b3a0aacb393b42c8196f2a93cc703510437a0d099afa5f5cc129f7120f0d763d24d8faca88b81356e750008fb6a6101aa15955aa96b58b3059e9bb9a4a93d56319261fc8ec53eff90eb5c46827731c3a1d18da30c306077f6c2fd0852f77e3885142745a',
+'isremember':'false',
+'callback':'successFun',
+'fingerprint':'33062264A11B40B7017592C42E30536DFE0CCEF6EB36E382_011',
+'path':'https://jianli.58.com/resumedetail/singles/3_nerXTGys_EmpnedNTvtalEDkTenX_EdNTvUknpsfnenY_EHY_eHYnetvlEOsnErsTisfTErf_6**?pts=1532782300884',
+'finger2':'zh-CN|24|1|4|1600_900|1600_860|-480|1|1|1|undefined|1|unknown|Win32|unknown|3|false|false|false|false|false|0_false_false|d41d8cd98f00b204e9800998ecf8427e|a8f8d2f174d04c4628d8a4f0a8ec9678',
+'username':'18123642476'
+}
+# data = "source=passport" \
+#        "password=28464a10a3cfd896df2063fc7f16167c30af6ff71aa4405428717cf16970d92883c2602ec73c77571aaa14eede5fce34feeda0b511cc2002c99f249c5d3c93394dcffa6e8c0fa004cefcd130c42d096a60b9faa9364b3008faeaa8447fced79f092653df5a24d78beafeaae88862f8bcc800b82e797a667bd01f4fbd1afef473" \
+#        "timesign=" \
+#        "isremember=false" \
+#        "callback=successFun" \
+#        "yzmstate=" \
+#        "fingerprint=33062264A11B40B7017592C42E30536DFE0CCEF6EB36E382_011" \
+#        "path=https%3A%2F%2Fjianli.58.com%2Fresumedetail%2Fsingles%2F3_nerXTGys_EmpnedNTvtalEDkTenX_EdNTvUknpsfnenY_EHY_eHYnetvlEOsnErsTisfTErf_6**%3Fpts%3D1532781531562&finger2=zh-CN%7C24%7C1%7C4%7C1600_900%7C1600_860%7C-480%7C1%7C1%7C1%7Cundefined%7C1%7Cunknown%7CWin32%7Cunknown%7C3%7Cfalse%7Cfalse%7Cfalse%7Cfalse%7Cfalse%7C0_false_false%7Cd41d8cd98f00b204e9800998ecf8427e%7Ca8f8d2f174d04c4628d8a4f0a8ec9678&username=18123642476&validcode=&vcodekey=&btnSubmit=%E7%99%BB%E5%BD%95%E4%B8%AD..."
+s = requests.Session()
+r1 = s.post(login,data=data)
+print(r1.text)
+print(r1.status_code)
+print(r1.cookies.items())
+r = s.get(url)
+print(r.text)
